3f33c2aa0b1186c2f009e766ddbc9ea03b4c6cb1
kash <cuisinealtair@gmail.com>
2024-06-07 22:13:18 +0900

    initial

---
+++ programming/ants-atcoder/src/ants_atcoder/ch2/2-3-1/p1.py
@@ -0,0 +1,14 @@
+N, W = map(int, input().split())
+VW = [list(map(int, input().split())) for _ in range(N)]
+
+dp = [[0] * (W + 1) for _ in range(N + 1)]
+
+for i in range(1, N + 1):
+    v, w = VW[i - 1]
+    for j in range(W + 1):
+        if j - w >= 0:
+            dp[i][j] = max(dp[i - 1][j - w] + v, dp[i - 1][j])
+        else:
+            dp[i][j] = dp[i - 1][j]
+
+print(max(dp[-1]))
