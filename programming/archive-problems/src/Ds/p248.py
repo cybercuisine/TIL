dcd0295644bd2864ece38830d7972f1129cba49e
kash <cuisinealtair@gmail.com>
2024-06-22 12:34:19 +0900

    wip

---
+++ programming/archive-problems/src/Ds/p248.py
@@ -0,0 +1,13 @@
+from bisect import bisect_left, bisect_right
+
+
+N = int(input())
+A = list(map(int, input().split()))
+pos = [[] for _ in range(N + 1)]
+for i, x in enumerate(A, 1):
+    pos[x].append(i)
+Q = int(input())
+for _ in range(Q):
+    L, R, X = map(int, input().split())
+    B = pos[X]
+    print(bisect_right(B, R) - bisect_left(B, L))
