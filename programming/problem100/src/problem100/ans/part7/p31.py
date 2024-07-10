72fe20261e78b3e1f9c76ae46a2c991d70929e8c
kash <cuisinealtair@gmail.com>
2024-07-10 12:54:11 +0900

    lint

--- programming/problem100/src/problem100/ans/part7/p31.py
+++ programming/problem100/src/problem100/ans/part7/p31.py
@@ -1,7 +1,7 @@
 import queue
 
-d_odd = ((-1,0), (1,0), (0,-1), (0,1), (1,-1), (1,1))
-d_even = ((-1,0), (1,0), (0,-1), (0,1), (-1,-1), (-1,1))
+d_odd = ((-1, 0), (1, 0), (0, -1), (0, 1), (1, -1), (1, 1))
+d_even = ((-1, 0), (1, 0), (0, -1), (0, 1), (-1, -1), (-1, 1))
 
 W, H = map(int, input().split())
 
@@ -19,15 +19,15 @@ while not q.empty():
     y, x = q.get()
     if visited[y][x] == 1:
         continue
-    
+
     visited[y][x] = 1
     if y % 2 == 0:
         d = d_even
     else:
         d = d_odd
-    for dx, dy in  d:
+    for dx, dy in d:
         if (0 <= x + dx < W + 2) and (0 <= y + dy < H + 2):
-            if mp[y+dy][x+dx] == 1:
+            if mp[y + dy][x + dx] == 1:
                 ans += 1
             else:
                 if visited[y + dy][x + dx] == 0:
